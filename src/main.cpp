a44fb6a15140524872cfb23d00636e1255364a3f
Mohammad Moradi <75095946+itismoradi@users.noreply.github.com>
2021-07-11 20:49:26 -0700

    Create main.cpp
    add extra doc comments

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+    BehBook
+    A program for better reading of books on certain days
+*/
+
+int main()
+{
+    return 0;
+}
+
+//Data Structures and Algorithms Course
+//at BuAli Sina university of Hamedan
+//coders : Mohammad Moradi, Erfan Ghaem Panah
+//summer 2021
