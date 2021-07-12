3e4dbe63f09683a8eaaa0d045d63834bc1025866
Erfan Gh <77532603+er4nGH@users.noreply.github.com>
2021-07-12 20:04:50 +0430

    Update node.hpp

--- include/node.hpp
+++ include/node.hpp
@@ -1,4 +1,11 @@
-struct Node {
-	int data;
-	Node* next;
-};
+class Node
+{
+    private :
+        int data; 
+        Node next; 
+    public :
+        int getData() ;
+        void setData(int);
+        void setNext(Node n);
+        Node getNext();
+}
