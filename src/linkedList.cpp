120050abc1a8933982d689ffc83daf289c7b0e4d
Erfan Gh <77532603+er4nGH@users.noreply.github.com>
2021-07-12 20:03:31 +0430

    update linked list

---
+++ src/linkedList.cpp
@@ -0,0 +1,84 @@
+class linkedList
+{
+    private :
+        Node frist;
+    public :
+        Node* InsertNode(int , double ) ;
+        void removeNode(double) ;
+        int  FindNode (double) ;
+        bool IsEmpty() ;
+
+}
+//////////////////////////////////////////////////////////
+
+bool linkedList::IsEmpty()
+{
+    return first.getNext() == null ;
+}   
+
+//////////////////////////////////////////////////////////
+
+Node* linkedList::InsertNode(int index, double x) 
+{
+    if (index < 0) return NULL;
+
+    int currIndex = 1;
+    Node* currNode = head;
+    
+    while (currNode && index > currIndex) {
+        currNode = currNode->next;
+        currIndex++;
+    }
+    if (index > 0 && currNode == NULL) return NULL;
+    Node* newNode = new Node;
+    newNode->data = x;
+    
+    if (index == 0) {
+        newNode->next = head;
+        head = newNode;
+    }
+    else {
+        newNode->next = currNode->next;
+        currNode->next = newNode;
+    }
+    return newNode;
+}
+//////////////////////////////////////////////////////////
+
+int linkedList::FindNode(double x)
+{
+    Node* currNode = head;
+    int currIndex = 1;
+    while (currNode && currNode->data != x) {
+        currNode = currNode->next;
+        currIndex++;
+    }
+    if (currNode) return currIndex;
+    return 0;
+}
+
+//////////////////////////////////////////////////////////
+
+int linkedList::removeNode(double x) {
+    Node* prevNode = NULL;
+    Node* currNode = head;
+    int currIndex = 1;
+    while (currNode && currNode->data != x) {
+        prevNode = currNode;
+        currNode = currNode->next;
+        currIndex++;
+    }
+    if (currNode) {
+        if (prevNode) {
+            prevNode->next = currNode->next;
+            delete currNode;
+        }
+        else {
+            head = currNode->next;
+            delete currNode;
+        }
+        return currIndex;
+    }
+    return 0;
+}
+//////////////////////////////////////////////////////////
