4e31d17e34d904d85b88aea7d46b8e6ef3d29ae0
Erfan Gh <77532603+er4nGH@users.noreply.github.com>
2021-07-12 20:05:20 +0430

    Update linkedList.hpp

---
+++ include/linkedList.hpp
@@ -0,0 +1,10 @@
+class linkedList
+{
+    private :
+        Node frist;
+    public :
+        Node* InsertNode(int , double ) ;
+        void removeNode(double) ;
+        int  FindNode (double) ;
+        bool IsEmpty() ;
+}
