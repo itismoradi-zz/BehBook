6d09f5b8e73b0360004cf5773fa014817f6491e0
Erfan Gh <77532603+er4nGH@users.noreply.github.com>
2021-07-13 12:30:47 +0430

    Update app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,5 @@
 #include "app.hpp"
+#include "linkedList.hpp"
 
 App::App()
 {
@@ -7,5 +8,20 @@ App::App()
 
 App::exec()
 {
+    int days, chapters, temp;
+
+	cout<<"Enter the number of days :"<<endl;
+	cin>>days;
+
+	cout<<"Enter the number of chapters :"<<endl;
+	cin>>chapters;
+
+    cout<<"Enter the number of pages per chapter :"<<endl;
+	for (size_t i = 0; i < chapters; i++)
+	{
+		cin>>temp;
+        insertNode(temp);
+	}
     
+    // ...
 }
