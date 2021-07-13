ad3127cec8c436fdb02d971be5b541c9495534aa
Mohammad Moradi <75095946+itismoradi@users.noreply.github.com>
2021-07-14 02:14:10 +0430

    move exec() local variables to App data members
    add getInformation function

--- include/app.hpp
+++ include/app.hpp
@@ -1,13 +1,20 @@
 #ifndef APP_IG
 #define APP_IG
 
+#include "linkedList.hpp"
+
 class App
 {
     public:
         App();
         int exec();
+        void getInformation();
     private:
         bool status;
+        unsigned int days;
+        unsigned int chapters;
+        unsigned int temp;
+        LinkedList pages;
 };
 
 #endif
