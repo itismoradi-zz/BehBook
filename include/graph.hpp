8af6f989b51bcde266fce8eed5c1eb25d8a65176
Mohammad Moradi <75095946+itismoradi@users.noreply.github.com>
2021-08-04 04:26:14 -0700

    add data members  and constructor

--- include/graph.hpp
+++ include/graph.hpp
@@ -2,13 +2,22 @@
 #define GRAPH_IG
 
 #include "linkedList.hpp"
+#include "readingPerDay.hpp"
+#include <vector>
 
 class Graph
 {
     public:
-
+        Graph(unsigned int *, unsigned int *, unsigned int *, std::vector<unsigned int> *);
     private:
         LinkedList ** list;
+        ReadingPerDay reading;
+        unsigned int * days;
+        unsigned int * chapters;
+        unsigned int * averageChapterPages;
+        std::vector<unsigned int> * ChaptersPages;
+    
+        void enterData();
 };
 
 #endif
