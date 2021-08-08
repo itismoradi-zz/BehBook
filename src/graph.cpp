ea4f5c49ac86f50a467a7e4f80031143ac7807ea
er4nGH <erfan.dweb@gmail.com>
2021-08-08 17:28:49 +0430

    update graph enterData function

--- src/graph.cpp
+++ src/graph.cpp
@@ -16,5 +16,16 @@ Graph::Graph(unsigned int * days, unsigned int * chapters, unsigned int * averag
 
 void Graph::enterData()
 {
-
+    for (size_t i = 1 ; i <= *chapters ; i++)
+    {
+        for (int e = i+1 ; e <= (*chapters +1) ; e++)
+        {
+            unsigned int sum = 0 ;
+            for (int m = i ; m<e ; m++)
+            {
+                sum+=ChaptersPages->at(m-1);
+            }
+            list[i-1].insertNode(e,sum);
+        }
+    }
 }
